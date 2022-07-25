90f9cc49edbd85140a8a58cf495784947a8583f9
muzammil-kt <muzammil@anjamerica.com>
2022-07-25 11:52:59 +0530

    design for register and login page

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ import Link from "next/link";
 
 export default function Home() {
   const handleSubmit = async () => {
-    console.log("submitted");
+    console.log("submitted successfully");
   };
   return (
     <div className={styles.container}>
